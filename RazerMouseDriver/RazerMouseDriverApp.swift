7f8bb9cb89d901359183a58b86d315289b96a223
Felipe Rugai <felipe.rugai@average.com.br>
2025-07-18 22:23:08 -0300

    Initial Commit

---
+++ RazerMouseDriver/RazerMouseDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RazerMouseDriverApp.swift
+//  RazerMouseDriver
+//
+//  Created by Felipe on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RazerMouseDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
